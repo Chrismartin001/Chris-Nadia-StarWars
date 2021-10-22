3341705722fe5a0027548fb3bff0f37e81a12972
Chris <christopher.martin5555@gmail.com>
2021-10-22 10:17:00 -0700

    Hi lots of JavaScript

---
+++ script/planets.js
@@ -0,0 +1,49 @@
+const apiURL = "https://swapi.dev/api/planets/";
+const planetContainer = document.querySelector(".container");
+
+const planets = [];
+
+axios
+  .get(`${apiURL}/1`)
+  .then((result) => {
+    console.log(result);
+    console.log(result.data);
+    planets.push(result.data);
+  })
+  .then((result) => {
+    const content = planets.forEach((element) => {
+      const singlePlanetContainer = document.createElement("div");
+      singlePlanetContainer.classList.add("card");
+
+      const title = document.createElement("h1");
+      title.classList.add("title");
+      title.innerText = `${element.name}`;
+
+      const climate = document.createElement("h2");
+      climate.classList.add("climate");
+      climate.innerText = `Climate: ${element.climate}`;
+
+      const terrain = document.createElement("p");
+      terrain.classList.add("terrain");
+      terrain.innerText = `Terrain: ${element.terrain}`;
+
+      const films = document.createElement("p");
+      films.classList.add("films");
+      films.innerText = element.films;
+
+      const population = document.createElement("p");
+      population.classList.add("population");
+      population.innerText = `Population: ${element.population}`;
+
+      singlePlanetContainer.appendChild(title);
+      singlePlanetContainer.appendChild(climate);
+      singlePlanetContainer.appendChild(terrain);
+      singlePlanetContainer.appendChild(films);
+      singlePlanetContainer.appendChild(population);
+      planetContainer.appendChild(singlePlanetContainer);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+console.log(planets);
